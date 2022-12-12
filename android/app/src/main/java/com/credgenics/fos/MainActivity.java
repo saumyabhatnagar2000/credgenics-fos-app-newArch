72cbd23e679cc1c5d19d551343f6e6d211604268
Saumya Bhatnagar <saumyabhatnagar@SaumyaMacBook-Air-2.local>
2022-12-12 16:30:20 +0530

    Fixed foreground service issues

--- android/app/src/main/java/com/credgenics/fos/MainActivity.java
+++ android/app/src/main/java/com/credgenics/fos/MainActivity.java
@@ -4,6 +4,12 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import android.os.Bundle;
+//foreground service imports 
+import android.content.Intent;
+import android.util.Log;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class MainActivity extends ReactActivity {
 
@@ -53,6 +59,49 @@ public class MainActivity extends ReactActivity {
       // React 18).
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+    }
+  }
+
+  public boolean isOnNewIntent = false;
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    isOnNewIntent = true;
+    ForegroundEmitter();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    if (isOnNewIntent == true) {
+    } else {
+      ForegroundEmitter();
+    }
+  }
+
+  public void ForegroundEmitter() {
+    // this method is to send back data from java to javascript so one can easily
+    // know which button from notification or the notification button is clicked
+    String main = getIntent().getStringExtra("mainOnPress");
+    String btn = getIntent().getStringExtra("buttonOnPress");
+    String btn2 = getIntent().getStringExtra("button2OnPress");
+    WritableMap map = Arguments.createMap();
+    if (main != null) {
+      map.putString("main", main);
+    }
+    if (btn != null) {
+      map.putString("button", btn);
+    }
+    if (btn2 != null) {
+      map.putString("button", btn);
+    }
+    try {
+      getReactInstanceManager().getCurrentReactContext()
+          .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+          .emit("notificationClickHandle", map);
+    } catch (Exception e) {
+      Log.e("SuperLog", "Caught Exception: " + e.getMessage());
     }
   }
 }
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
 /**
  * @format
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
+ReactNativeForegroundService.register();
 AppRegistry.registerComponent(appName, () => App);
