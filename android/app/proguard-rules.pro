e52e6e7a25b22afb765bfda0a88f44c4e25e60e0
Saumya Bhatnagar <saumyabhatnagar@SaumyaMacBook-Air-2.local>
2022-12-14 17:44:28 +0530

    Fixed proguard rules in release build, added babel to remove console logs

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -11,3 +11,11 @@
 
 -keep class com.swmansion.reanimated.** { *; }
 -keep class com.facebook.react.turbomodule.** { *; }
+
+
+-keep public class com.horcrux.svg.** {*;}
+
+-keepclassmembers enum * {*;}
+
+-keep class com.facebook.hermes.unicode.** { *; }
+-keep class com.facebook.jni.** { *; }
--- babel.config.js
+++ babel.config.js
@@ -1,44 +1,42 @@
 var path = require('path');
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
+    presets: ['module:metro-react-native-babel-preset'],
+    env: {
+        production: {
+            plugins: ['react-native-paper/babel'],
+            plugins: ['transform-remove-console']
+        }
     },
-  },
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['.'],
-        resolvePath(sourcePath, currentFile, opts) {
-          if (
-            sourcePath === 'react-native' &&
-            !(
-              (
-                currentFile.includes('node_modules/react-native/') || // macos/linux paths
-                currentFile.includes('node_modules\\react-native\\')
-              ) // windows path
-            ) &&
-            !(
-              currentFile.includes('resolver/react-native/') ||
-              currentFile.includes('resolver\\react-native\\')
-            )
-          ) {
-            return path.resolve(__dirname, 'resolver/react-native');
-          }
-          /**
-           * The `opts` argument is the options object that is passed through the Babel config.
-           * opts = {
-           *   extensions: [".js"],
-           *   resolvePath: ...,
-           * }
-           */
-          return undefined;
-        },
-      },
-    ],
-    'react-native-reanimated/plugin',
-  ],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['.'],
+                resolvePath(sourcePath, currentFile, opts) {
+                    if (
+                        sourcePath === 'react-native' &&
+                        !(
+                            (
+                                currentFile.includes(
+                                    'node_modules/react-native/'
+                                ) || // macos/linux paths
+                                currentFile.includes(
+                                    'node_modules\\react-native\\'
+                                )
+                            ) // windows path
+                        ) &&
+                        !(
+                            currentFile.includes('resolver/react-native/') ||
+                            currentFile.includes('resolver\\react-native\\')
+                        )
+                    ) {
+                        return path.resolve(__dirname, 'resolver/react-native');
+                    }
+                    return undefined;
+                }
+            }
+        ],
+        'react-native-reanimated/plugin'
+    ]
 };
